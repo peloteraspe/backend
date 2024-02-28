fd345ff95abf3389c7b79a9d1053a93e859fb9fd
rodrigogeorgetti <rodrigogeorgetti.cba@gmail.com>
2024-02-28 12:07:51 -0300

    corregido para que muestre el username de la tabla profile

--- Server/src/services/event.service.ts
+++ Server/src/services/event.service.ts
@@ -1,13 +1,22 @@
 import { supabase } from "../app";
 
 const getUsernameByEventIdService = async (eventId: string) => {
-  const { data: userId } = await supabase
+  const response = await supabase
     .from("event")
     .select("created_by_id")
     .eq("id", eventId)
     .single();
 
-  if (!userId) throw new Error("El id ingresado no existe");
-  return userId;
+  if (response.error) {
+    throw new Error("El id ingresado no existe");
+  } else {
+    const createdById = response.data.created_by_id;
+    const { data: userId } = await supabase
+      .from("profile")
+      .select("username")
+      .eq("user", createdById)
+      .single();
+    return userId;
+  }
 };
 export { getUsernameByEventIdService };
