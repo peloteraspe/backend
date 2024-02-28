4a8c4ca06a846e22559395bde402e517bf447f1c
Pablo Bellido <bellido.peru26@gmail.com>
2024-02-28 17:28:20 -0500

    feat: PLTRS-4 Add config supabase module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SupabaseModule } from 'nestjs-supabase-js';
 
 import { EnvConfiguration, JoiValidationSchema } from './config';
 
@@ -10,6 +11,14 @@ import { EnvConfiguration, JoiValidationSchema } from './config';
       envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
+    }),
+    SupabaseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        supabaseUrl: configService.get('supabase.url') ?? '',
+        supabaseKey: configService.get('supabase.key') ?? '',
+      }),
     }),
   ],
 })
