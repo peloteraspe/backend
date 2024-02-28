75c087150fce590460530defae49b80bebc150e6
Pablo Bellido <bellido.peru26@gmail.com>
2024-02-28 17:12:12 -0500

    feat: PLTRS-4 Add supabase env

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,4 +3,8 @@ export const EnvConfiguration = () => ({
     environment: process.env.NODE_ENV ?? 'development',
     port: parseInt(process.env.PORT ?? '3005', 10) ?? 3000,
   },
+  supabase: {
+    url: process.env.SUPABASE_URL ?? '',
+    key: process.env.SUPABASE_KEY ?? '',
+  },
 });
