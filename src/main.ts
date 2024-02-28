1e65e30f7a47537f194f5021dd57415ef4930bef
Pablo Bellido <bellido.peru26@gmail.com>
2024-02-28 16:41:06 -0500

    refactor: PLTRS-4 Add logger to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
 
   /* ======= LOAD CONFIG .ENV.* ======= */
   const config: ConfigService = app.get(ConfigService);
 
   await app.listen(config.get('api.port') ?? 3005, '0.0.0.0');
-  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
+  logger.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
